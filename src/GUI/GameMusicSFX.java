24e77c66c7fec95429488ebd5c24d921f735d72f
drei <gian_martin@dlsu.edu.ph>
2022-12-10 13:18:21 +0800

    Commit

--- src/GUI/GameMusicSFX.java
+++ src/GUI/GameMusicSFX.java
@@ -34,7 +34,7 @@ public class GameMusicSFX {
 				while (clip.getMicrosecondLength() < clip.getMicrosecondPosition()) {}
 			}
 			else {
-				System.out.println("Cant find");
+				System.out.println("Can't find music file.");
 			}
 			
 		} catch (Exception ex){
@@ -42,30 +42,3 @@ public class GameMusicSFX {
 		}
 	}
 }
-
-/*
-try {
-    plowButtonPanel.setButtonSFX(MusicSFXLocation.PLOW_SFX_NAME);
-    shovelButtonPanel.setButtonSFX(MusicSFXLocation.SHOVEL_SFX_NAME);
-    pickaxeButtonPanel.setButtonSFX(MusicSFXLocation.PICKAXE_SFX_NAME);
-    plantButtonPanel.setButtonSFX(MusicSFXLocation.PLANT_SFX_NAME);
-    waterButtonPanel.setButtonSFX(MusicSFXLocation.WATER_SFX_NAME);
-    // fertilizerButtonPanel.setButtonSFX(PictureLocations.PLOW_ICON_NAME);
-    harvestButtonPanel.setButtonSFX(MusicSFXLocation.HARVEST_SFX_NAME);
-} catch (Exception ex)
-{
-    System.out.println(ex);
-}
-
-GameMusicSFX sfxPlayer = new GameMusicSFX();
-
-this.selectedButton = selectedButton;
-this.selectedButton.setBackground(SELECTED_TOOL_COLOR);
-
-for (ToolButtonPanel toolButtonPanel : toolButtonPanels) {
-    if (toolButtonPanel.getToolButton() == selectedButton) {
-    	sfxPlayer.playSFX(toolButtonPanel.getSFXPath());
-    	// System.out.println(toolButtonPanel.getSFXPath());
-    }
-}
-*/
